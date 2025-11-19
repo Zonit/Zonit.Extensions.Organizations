952d4960232af650100620bc6f47977782033481
Alerinos <42449535+Alerinos@users.noreply.github.com>
2025-11-19 02:41:52 +0100

    Fix IOrganizationProvider

---
+++ Source/Zonit.Extensions.Organizations.Abstractions/Providers/IOrganizationProvider.cs
@@ -0,0 +1,29 @@
+﻿namespace Zonit.Extensions.Organizations;
+
+/// <summary>
+/// 
+/// </summary>
+public interface IOrganizationProvider
+{
+    /// <summary>
+    /// Gets detailed information about an organization by ID.
+    /// Returns null if the organization does not exist or the module is disabled.
+    /// </summary>
+    /// <param name="organizationId"></param>
+    /// <returns></returns>
+    public Task<OrganizationModel?> GetOrganizationAsync(Guid organizationId);
+
+    /// <summary>
+    /// Gets the default organization for the specified user.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    public Task<OrganizationModel?> GetUserOrganizationAsync(Guid userId);
+
+    /// <summary>
+    /// Gets all organizations the specified user belongs to.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    public Task<IReadOnlyCollection<OrganizationModel>?> GetUserOrganizationsAsync(Guid userId);
+}
